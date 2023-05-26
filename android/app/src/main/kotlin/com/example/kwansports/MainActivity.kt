7fd9df47af54f48aaf0d96a5688bd38db7a07c12
vishalshukla <vishal.shukla@virtueanalytics.com>
2023-05-26 12:47:20 +0530

    project starter

---
+++ android/app/src/main/kotlin/com/example/kwansports/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kwansports
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/virtueanalytics/kwansports/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.virtueanalytics.kwansports
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
